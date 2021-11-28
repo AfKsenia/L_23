3806195382cc8006b0d268d2678f3654df5f3c30
AfKsenia <afksenia@gmail.com>
2021-11-28 18:57:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine($"Факториал числа {n} = {FactorialAsync(n).Result}");
+            Console.ReadKey();
+        }
+        static long Factorial(int n)
+        {
+            long res = 1;
+            for (int i = n; i >0; i--)
+            {
+                res *= i;
+            }
+            return res;
+        }
+        static async Task<long> FactorialAsync(int n)
+        {
+            long result = await Task.Run(() => Factorial(n));
+            return result;
+        }
+    }
+}
